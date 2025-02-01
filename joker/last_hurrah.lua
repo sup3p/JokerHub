15ae62f30d402cfc0b7e7994ebe5eec2a19327c4
sup3p <rayraydistefano@gmail.com>
2025-02-01 15:08:26 -0500

    Fix decimal retrigger values on Last Hurrah

--- joker/last_hurrah.lua
+++ joker/last_hurrah.lua
@@ -22,7 +22,7 @@ SMODS.Joker {
 	},
 	rarity = 3,
 	loc_vars = function(self, info_queue, card)
-		return {vars = {card.ability.extra.dollars_per_trigger, card.ability.extra.max_retriggers, clamp(G.GAME.dollars / card.ability.extra.dollars_per_trigger, 0, card.ability.extra.max_retriggers)}}
+		return {vars = {card.ability.extra.dollars_per_trigger, card.ability.extra.max_retriggers, clamp(math.floor(G.GAME.dollars / card.ability.extra.dollars_per_trigger, 0, card.ability.extra.max_retriggers))}}
 	end,
 	atlas = "atlas_jokers",
 	pos = { x = 3, y = 1 },
@@ -36,7 +36,7 @@ SMODS.Joker {
 		if not card.debuff and G.jokers.cards[#G.jokers.cards] == card then
 			if context.repetition and context.cardarea == G.play and G.GAME.dollars > 0 then
 				if context.other_card == context.scoring_hand[#context.scoring_hand] then
-					local retriggers = clamp(G.GAME.dollars / card.ability.extra.dollars_per_trigger, 0, card.ability.extra.max_retriggers)
+					local retriggers = clamp(math.floor(G.GAME.dollars / card.ability.extra.dollars_per_trigger, 0, card.ability.extra.max_retriggers))
 					return {
                         message = localize('k_again_ex'),
                         repetitions = retriggers,
